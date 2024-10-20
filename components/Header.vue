5f1a4f8e534b974f781d3d56068b00e1dfa0792a
Xman1923 <161951087+Xman1923@users.noreply.github.com>
2024-10-20 11:39:00 +0300

    Update

---
+++ components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="header">
+    <h1 class="logo">Casino X</h1>
+    <nav>
+      <ul class="nav-links">
+        <li><NuxtLink to="/">Ana Sayfa</NuxtLink></li>
+        <li><NuxtLink to="/games">Oyunlar</NuxtLink></li>
+        <li><NuxtLink to="/about">Hakkında</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  background-color: #0c0c0c;
+  color: white;
+  padding: 20px;
+}
+
+.logo {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.nav-links {
+  list-style: none;
+  display: flex;
+  margin-left: auto;
+}
+
+.nav-links li {
+  margin-left: 20px;
+}
+
+.nav-links li a {
+  color: white;
+  text-decoration: none;
+  font-weight: 500;
+}
+</style>
