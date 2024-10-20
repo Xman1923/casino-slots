e2427c723b6357183a8d03b9441ba4393ef8fd12
Xman1923 <161951087+Xman1923@users.noreply.github.com>
2024-10-20 11:49:34 +0300

    Update index.vue

--- pages/games/index.vue
+++ pages/games/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="games">
     <h2>Oyunlar</h2>
-    <div class="games-list">
+    <div v-if="loading" class="loading">Yükleniyor...</div>
+    <div v-else class="games-list">
       <GameCard v-for="game in games" :key="game.id" :game="game" />
     </div>
   </div>
@@ -12,23 +13,37 @@ import GameCard from '~/components/GameCard.vue';
 
 export default {
   components: {
-    GameCard
+    GameCard,
   },
   data() {
     return {
-      games: [
-        { id: 1, name: 'Blackjack', description: 'Klasik blackjack oyunu.' },
-        { id: 2, name: 'Rulet', description: 'Şansınıza güvenin! Rulet oynayın.' },
-        { id: 3, name: 'Poker', description: 'Rakiplerinizi yenin ve kazanın.' }
-      ]
+      games: [],
+      loading: true,
     };
-  }
+  },
+  async mounted() {
+    // Simulate API call
+    setTimeout(() => {
+      this.games = [
+        { id: 1, name: 'Blackjack', description: 'Klasik blackjack oyunu.', image: '/images/blackjack.jpg' },
+        { id: 2, name: 'Rulet', description: 'Şansınıza güvenin! Rulet oynayın.', image: '/images/roulette.jpg' },
+        { id: 3, name: 'Poker', description: 'Rakiplerinizi yenin ve kazanın.', image: '/images/poker.jpg' },
+      ];
+      this.loading = false;
+    }, 2000);
+  },
 }
 </script>
 
 <style scoped>
 .games {
   padding: 20px;
+}
+
+.loading {
+  font-size: 18px;
+  text-align: center;
+  color: #888;
 }
 
 .games-list {
