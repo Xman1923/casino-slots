1c616c077332dd19db212cfb53b634abfe15e227
Xman1923 <161951087+Xman1923@users.noreply.github.com>
2024-10-20 11:41:48 +0300

    Update

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+/* Genel stil ayarları */
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+</style>
