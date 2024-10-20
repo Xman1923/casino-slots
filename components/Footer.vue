8dc0977740a67ddd2d3b75995f96dc4d4bc15dd2
Xman1923 <161951087+Xman1923@users.noreply.github.com>
2024-10-20 11:39:55 +0300

    Update

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <p>© 2024 Casino X. Tüm Hakları Saklıdır.</p>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background-color: #0c0c0c;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
+</style>
