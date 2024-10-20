067a7b79cbab15fff0e6ff111cd1e9de4013edfc
Xman1923 <161951087+Xman1923@users.noreply.github.com>
2024-10-20 11:43:41 +0300

    Update

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h2>Hakkında</h2>
+    <p>Casino X, en iyi casino oyunlarını sunmak için burada!</p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  text-align: center;
+  padding: 50px 0;
+}
+</style>
